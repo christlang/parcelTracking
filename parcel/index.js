a431c8717f466e021e15f728f6b259746d3a3035
Christian Lange <lange@synyx.de>
2018-09-30 01:12:29 +0200

    fix: first route that matches takes the traffic
    /:x? will take everything like /x /y /form ...

--- parcel/index.js
+++ parcel/index.js
@@ -7,10 +7,11 @@ const {
     saveAction
 } = require('./controller');
 
-router.get('/:orderBy?', listAction);
+
 router.get('/archive/:id', archiveAction);
 router.get('/unarchive/:id', unarchiveAction);
 router.get('/form/:id?', formAction);
 router.post('/save', saveAction);
+router.get('/:orderBy?', listAction);
 
 module.exports = router;
