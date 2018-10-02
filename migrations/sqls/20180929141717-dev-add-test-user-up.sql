2d1474f84fd44e513d6dcce8d53466582808dc10
Christian Lange <lange@synyx.de>
2018-10-02 20:23:21 +0200

    corrected user-test data for dev environment

--- migrations/sqls/20180929141717-dev-add-test-user-up.sql
+++ migrations/sqls/20180929141717-dev-add-test-user-up.sql
@@ -6,10 +6,15 @@ INSERT INTO 'Users' (`firstname`, `lastname`, `username`, `password`) VALUES
     'test',
     'test',
     'test1',
-    '$2a$10$ZweZbb3TIOtJqHW4O9.ZD.2.Ht7klIYNvlHdDsGyKtp4Von75uurK'
+    '$2a$10$yeDornCgneGogOxwHY963Oe/T46kG4VKMpEu9uZqBi//Vme4Q0y7i'
   ), (
     'test',
     'test',
     'test2',
+    '$2a$10$wgn6cQToxqLSP0zyeWicN.6IDxq/Ot/iz7wxPpaBV7tLtVrk4ftLO'
+  ), (
+    'test',
+    'test',
+    'test',
     '$2a$10$9BlV00cMao5.YJlcIbVvu.yVuxwFW3WJddEn85yEwFLFZjjP4P4xK'
   );
