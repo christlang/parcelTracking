a80b90ca2760f5d7e26d816ba045003fc0a95eb2
Christian Lange <lange@synyx.de>
2018-10-04 15:12:09 +0200

    testcafe: added logout test

--- test/testcafe/usecases/parcels.js
+++ test/testcafe/usecases/parcels.js
@@ -8,6 +8,25 @@ fixture `parcels`
     .page `http://localhost:${port}`;
 
 
+
+
+test('logout', async t => {
+    const login = new Login(t);
+    const browser = new Browser(t);
+
+    await login.enterUser('test');
+    await login.enterPass('test');
+    await login.submit();
+
+    const parcels = new Parcels(t);
+    await parcels.logout();
+
+    const location = await browser.getLocation();
+
+    await t.expect(location.pathname).eql('/login.html');
+});
+
+
 test('edit parcel and just save should not change field arrived', async t => {
     const login = new Login(t);
     const browser = new Browser(t);
